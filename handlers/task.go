bd05e2bab4c46c687056605ecfe3e527fe86611d
KushakovKH <Kushakov-KH@yandex.ru>
2024-10-08 21:55:55 +0300

    first conmit

---
+++ handlers/task.go
@@ -0,0 +1,349 @@
+package handlers
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"go_final_project/database"
+)
+
+func TaskHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		handlePostTask(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func handlePostTask(w http.ResponseWriter, r *http.Request) {
+	var task database.Task
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&task)
+	if err != nil {
+		http.Error(w, `{"error":"Ошибка десериализации JSON"}`, http.StatusBadRequest)
+		return
+	}
+
+	if task.Title == "" {
+		http.Error(w, `{"error":"Не указан заголовок задачи"}`, http.StatusBadRequest)
+		return
+	}
+
+	dateFormat := "20060102"
+	var date time.Time
+	if task.Date == "" {
+		date = time.Now()
+		task.Date = date.Format(dateFormat)
+	} else {
+		date, err = time.Parse(dateFormat, task.Date)
+		if err != nil {
+			http.Error(w, `{"error":"Дата представлена в формате, отличном от 20060102"}`, http.StatusBadRequest)
+			return
+		}
+	}
+
+	if task.Repeat != "" {
+		if err := validateRepeatPattern(task.Repeat); err != nil {
+			http.Error(w, fmt.Sprintf(`{"error":"%v"}`, err), http.StatusBadRequest)
+			return
+		}
+	}
+
+	today := time.Now().Format(dateFormat)
+	if date.Format(dateFormat) < today {
+		if task.Repeat == "" {
+			task.Date = today
+		} else {
+			nextDate, err := NextDate(time.Now(), task.Date, task.Repeat)
+			if err != nil {
+				http.Error(w, fmt.Sprintf(`{"error":"%v"}`, err), http.StatusBadRequest)
+				return
+			}
+			task.Date = nextDate
+		}
+	}
+
+	res, err := database.DB.Exec("INSERT INTO scheduler (date, title, comment, repeat) VALUES (?, ?, ?, ?)",
+		task.Date, task.Title, task.Comment, task.Repeat)
+	if err != nil {
+		http.Error(w, `{"error":"Ошибка в базе данных"}`, http.StatusInternalServerError)
+		return
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		http.Error(w, `{"error":"Ошибка в базе данных"}`, http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]string{"id": strconv.FormatInt(id, 10)}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	json.NewEncoder(w).Encode(response)
+}
+
+func splitRepeatPattern(pattern string) []string {
+	return strings.Fields(pattern)
+}
+
+func validateRepeatPattern(pattern string) error {
+	parts := splitRepeatPattern(pattern)
+	if len(parts) < 1 {
+		return fmt.Errorf("Неподдерживаемый шаблон")
+	}
+
+	unit := parts[0]
+	switch unit {
+	case "d", "w", "m", "y":
+		if len(parts) > 1 {
+			if _, err := strconv.Atoi(parts[1]); err != nil {
+				return fmt.Errorf("Invalid repeat value")
+			}
+		} else if unit == "w" && len(parts) == 1 {
+			return fmt.Errorf("Invalid repeat value for weekly repeat")
+		}
+	default:
+		return fmt.Errorf("Неподдерживаемый шаблон")
+	}
+
+	return nil
+}
+
+func NextDate(now time.Time, date string, repeat string) (string, error) {
+	startDate, err := time.Parse("20060102", date)
+	if err != nil {
+		return "", fmt.Errorf("Недопустимый формат даты: %w", err)
+	}
+
+	if repeat == "" {
+		return "", errors.New("повторное правило не может быть пустым")
+	}
+
+	parts := strings.Split(repeat, " ")
+	if len(parts) == 0 {
+		return "", errors.New("Недопустимый формат правила повтора")
+	}
+
+	switch parts[0] {
+	case "d":
+		if len(parts) != 2 {
+			return "", errors.New("Недопустимый формат правила повтора для ежедневного правила")
+		}
+		days, err := strconv.Atoi(parts[1])
+		if err != nil || days <= 0 || days > 400 {
+			return "", errors.New("недопустимый дневной интервал")
+		}
+		nextDate := startDate.AddDate(0, 0, days)
+		for !nextDate.After(now) {
+			nextDate = nextDate.AddDate(0, 0, days)
+		}
+		return nextDate.Format("20060102"), nil
+	case "y":
+		if len(parts) != 1 {
+			return "", errors.New("Недопустимый формат правила повторения для годового правила")
+		}
+		nextDate := startDate.AddDate(1, 0, 0)
+		for !nextDate.After(now) {
+			nextDate = nextDate.AddDate(1, 0, 0)
+		}
+		return nextDate.Format("20060102"), nil
+	default:
+		return "", errors.New("Неподдерживаемое правило повтора")
+	}
+}
+
+func NextDateHandler(w http.ResponseWriter, r *http.Request) {
+	nowStr := r.FormValue("now")
+	dateStr := r.FormValue("date")
+	repeatStr := r.FormValue("repeat")
+
+	now, err := time.Parse("20060102", nowStr)
+	if err != nil {
+		http.Error(w, "Недопустимый формат даты 'now'", http.StatusBadRequest)
+		return
+	}
+
+	nextDate, err := NextDate(now, dateStr, repeatStr)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprint(w, nextDate)
+}
+
+func NextDayCalculation() {
+	now := time.Date(2024, time.January, 26, 0, 0, 0, 0, time.UTC)
+	date := "20240229"
+	repeat := "y"
+
+	nextDate, err := NextDate(now, date, repeat)
+	if err != nil {
+		log.Fatalf("Error calculating next date: %v", err)
+	}
+	fmt.Printf("Next Date: %s\n", nextDate)
+}
+func HandleTaskID(repository *database.Repository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.URL.Query().Get("id")
+		if id == "" {
+			http.Error(w, `{"error":"Не указан идентификатор"}`, http.StatusBadRequest)
+			return
+		}
+
+		task, err := repository.GetTaskByID(id)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, `{"error":"Задача не найдена"}`, http.StatusNotFound)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(task)
+	}
+}
+
+func HandleTaskDelete(repository *database.Repository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		id := r.URL.Query().Get("id")
+		if id == "" {
+			http.Error(w, `{"error":"Идентификатор задачи не указан"}`, http.StatusBadRequest)
+			return
+		}
+
+		if err := repository.DeleteTask(id); err != nil {
+			log.Println(err)
+			http.Error(w, `{"error":"Ошибка в базе данных"}`, http.StatusInternalServerError)
+			return
+		}
+
+		json.NewEncoder(w).Encode(map[string]interface{}{})
+	}
+}
+
+func HandleTaskDone(repository *database.Repository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		id := r.URL.Query().Get("id")
+		if id == "" {
+			http.Error(w, `{"error":"Идентификатор задачи не указан"}`, http.StatusBadRequest)
+			return
+		}
+
+		task, err := repository.GetTaskByID(id)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, `{"error":"Задача не найдена"}`, http.StatusNotFound)
+			return
+		}
+
+		if task.Repeat == "" {
+			if err := repository.DeleteTask(id); err != nil {
+				log.Println(err)
+				http.Error(w, fmt.Sprintf(`{"error":"%s"}`, err.Error()), http.StatusInternalServerError)
+				return
+			}
+		} else {
+			nextDate, err := NextDate(time.Now(), task.Date, task.Repeat)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, fmt.Sprintf(`{"error":"%s"}`, err.Error()), http.StatusBadRequest)
+				return
+			}
+
+			task.Date = nextDate
+			if err := repository.UpdateTask(*task); err != nil {
+				log.Println(err)
+				http.Error(w, fmt.Sprintf(`{"error":"%s"}`, err.Error()), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		json.NewEncoder(w).Encode(map[string]interface{}{})
+	}
+}
+
+func HandleTaskPut(repository *database.Repository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var task database.Task
+		if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+			log.Println(err)
+			http.Error(w, `{"error":"Некорректный JSON"}`, http.StatusBadRequest)
+			return
+		}
+
+		if task.ID == "" {
+			http.Error(w, `{"error":"Не указан идентификатор"}`, http.StatusBadRequest)
+			return
+		}
+
+		if task.Date == "" || task.Title == "" {
+			http.Error(w, `{"error":"Не указаны дата и заголовок"}`, http.StatusBadRequest)
+			return
+		}
+
+		if err := validateDate(&task); err != nil {
+			http.Error(w, fmt.Sprintf(`{"error":"%s"}`, err.Error()), http.StatusBadRequest)
+			return
+		}
+
+		if err := repository.UpdateTask(task); err != nil {
+			log.Println(err)
+			http.Error(w, `{"error":"Задача не найдена"}`, http.StatusNotFound)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]interface{}{})
+	}
+}
+
+func validateDate(task *database.Task) error {
+	parsedDate, err := time.Parse("20060102", task.Date)
+	if err != nil {
+		return errors.New("Некорректный формат даты")
+	}
+	today := time.Now().Format("20060102")
+	if parsedDate.Before(time.Now()) {
+		if task.Repeat == "" {
+			task.Date = today
+		} else {
+			nextDate, err := NextDate(time.Now(), task.Date, task.Repeat)
+			if err != nil {
+				return fmt.Errorf("Ошибка при расчете следующей даты: %w", err)
+			}
+			task.Date = nextDate
+		}
+	}
+
+	return nil
+}
+
+func HandleTaskGet(repository *database.Repository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		search := r.URL.Query().Get("search")
+		tasks, err := repository.GetTasks(search)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, `{"error": "Ошибка получения задач"}`, http.StatusInternalServerError)
+			return
+		}
+
+		if tasks == nil {
+			tasks = []database.Task{}
+		}
+
+		response := map[string][]database.Task{
+			"tasks": tasks,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	}
+}
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"go_final_project/database"
+	"go_final_project/handlers"
+
+	"github.com/go-chi/chi/v5"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := database.InitializeDB("scheduler.db")
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+	defer db.Close()
+
+	repository := database.NewRepository(db)
+
+	startServer(repository)
+}
+
+func startServer(repository *database.Repository) {
+	cNR := chi.NewRouter()
+
+	directoryPath := "web"
+	fileServer := http.FileServer(http.Dir(directoryPath))
+	cNR.Handle("/*", fileServer)
+
+	cNR.Get("/api/nextdate", handlers.NextDateHandler)
+	cNR.Post("/api/task", handlers.TaskHandler)
+	cNR.Get("/api/tasks", handlers.HandleTaskGet(repository))
+	cNR.Put("/api/task", handlers.HandleTaskPut(repository))
+	cNR.Delete("/api/task", handlers.HandleTaskDelete(repository))
+	cNR.Get("/api/task", handlers.HandleTaskID(repository))
+	cNR.Post("/api/task/done", handlers.HandleTaskDone(repository))
+
+	port := ":7540"
+	err := http.ListenAndServe(port, cNR)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
